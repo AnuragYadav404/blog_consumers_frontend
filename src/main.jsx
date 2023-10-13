15c27738939ad27a8a7f23b25a7a3091ba6cc8d1
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-10-13 20:06:19 +0530

    Adds react-router-dom and localforage to dependencies. Adds code for main.jsx realated to ReactDOM root setup.

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 // here it sets the root of the ReactDOM
 // equal to the root element defined in ../index.html
 // within this ReactDOM root we render <App />
+// root renders App
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
